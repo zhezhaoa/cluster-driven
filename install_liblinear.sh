50584ebcb7a0e191c5b6ce7893e6fb08a6e1fb1a
zhezhaoa <1152543959@qq.com>
2016-11-30 22:02:43 +0800

    Create install_liblinear.sh

---
+++ install_liblinear.sh
@@ -0,0 +1,8 @@
+wget https://www.csie.ntu.edu.tw/~cjlin/liblinear/oldfiles/liblinear-1.96.zip
+unzip liblinear-1.96.zip
+rm liblinear-1.96.zip
+cd liblinear-1.96
+make
+chmod +x train
+chmod +x predict
+cd ..
